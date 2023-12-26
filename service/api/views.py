53011dc170e97fd7cacc1291ce269d1400f00939
dsborisovv <ds.borisov@yandex.ru>
2023-12-26 23:37:33 +0300

    Add hw-6

--- service/api/views.py
+++ service/api/views.py
@@ -10,6 +10,15 @@ from service.api.exceptions import AuthorizationError, ModelNotFoundError, UserN
 from service.log import app_logger
 
 
+POPULAR_RECOS = [202457, 193123, 132865, 122119, 91167, 74803, 68581, 55043, 45367, 40372]
+with open('service/data/tfidf_userknn_popular_model_1.json', "r", encoding="utf8") as file:
+    TFIDF_USERKNN_POPULAR_RECOS = json.loads(file.read())
+with open('service/data/lightfm_recos_final_2.json', "r", encoding="utf8") as file:
+    LIGHTFM_RECOS = json.loads(file.read())
+with open('service/data/ranker.json', "r") as file:
+    RANKER_RECOS = json.loads(file.read())
+
+
 class RecoResponse(BaseModel):
     user_id: int
     items: List[int]
@@ -53,17 +62,14 @@ async def get_reco(
         raise AuthorizationError(error_message="Invalid token")
 
     k_recs = request.app.state.k_recs
-    with open('service/data/tfidf_userknn_popular_model_1.json', "r", encoding="utf8") as file:
-        TFIDF_USERKNN_POPULAR_RECOS = json.loads(file.read())
-    with open('service/data/lightfm_recos_final_2.json', "r", encoding="utf8") as file:
-        LIGHTFM_RECOS = json.loads(file.read())
-    POPULAR_RECOS = [202457, 193123, 132865, 122119, 91167, 74803, 68581, 55043, 45367, 40372]
     if model_name == 'dummy_model':
         reco = list(range(k_recs))
     elif model_name == 'tfidf_userknn_popular_model':
         reco = TFIDF_USERKNN_POPULAR_RECOS.get(str(user_id), POPULAR_RECOS)
     elif model_name == 'lightfm_model':
         reco = LIGHTFM_RECOS.get(str(user_id), POPULAR_RECOS)
+    elif model_name == 'ranker':
+        reco = RANKER_RECOS.get(str(user_id), POPULAR_RECOS)
     return RecoResponse(user_id=user_id, items=reco)
 
 
